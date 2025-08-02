82b733ffb768ba9fa0a9c659835d0094d30e6fe2
jonathan <jonathan20cba@gmail.com>
2025-08-02 20:45:34 -0300

    Se define modelo de usuario

---
+++ user-service/src/entities/user.entity.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "string",
+    unique: true,
+    nullable: false,
+    length: 50,
+  })
+  username: string;
+
+  @Column({
+    type: "string",
+    unique: true,
+    nullable: false,
+    length: 50,
+  })
+  email: string;
+
+  @CreateDateColumn({
+    type: "timestamptz",
+    nullable: false,
+    default: () => "NOW()",
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: "timestamptz",
+    nullable: false,
+    default: () => "NOW()",
+  })
+  updatedAt: Date;
+}
